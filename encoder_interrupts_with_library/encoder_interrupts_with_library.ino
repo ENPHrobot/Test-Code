441ea4e79770e8ec27471c0c9a1e539b278efd58
Andy Tu <andytu@alumni.ubc.ca>
2015-07-07 12:04:13 -0700

    test updated interrupts library

--- encoder_interrupts_with_library/encoder_interrupts_with_library.ino
+++ encoder_interrupts_with_library/encoder_interrupts_with_library.ino
@@ -5,46 +5,38 @@
 
 // Sensor Ports
 #define ENCODER_A 0
- 
-void (*func_ptr) (void) = incre;
-Interrupts interrupt;
-volatile unsigned int encount = 0;
 
-// ISR function
-ISR(INT0_vect) {callfn();};
+volatile unsigned int encount = 0;
 
 void setup()
 {
-  #include <phys253setup.txt>
-  LCD.clear();
-  LCD.home();
-  
-  interrupt.enableExternalInterrupt(INT0, RISING);
-  
-  LCD.print("Press Start.");
-  while(!startbutton()){};
-  LCD.clear();
+#include <phys253setup.txt>
+    LCD.clear(); LCD.home();
+
+    enableExternalInterrupt(INT0, RISING);
+    attachISR(INT0, incre);
+
+    LCD.print("Press Start.");
+    while (!startbutton()) {};
+    LCD.clear();
 }
 
 void loop()
 {
-  while (startbutton()) {
-    if (encount > 100) func_ptr = incre2;
-    encount = 0;
-  }
-  LCD.clear();
-  LCD.print(encount);
-  delay(100);
+    while (startbutton()) {
+        delay(250);
+        if (encount > 100) attachISR(INT0, decre);
+        else attachISR(INT0, incre);
+    }
+    LCD.clear();
+    LCD.print(encount);
+    delay(100);
 }
 
 void incre() {
-  encount++;
+    encount++;
 }
 
-void incre2() {
-  encount += 20;
-}
-
-void callfn(){
-  func_ptr();
+void decre() {
+    encount--;
 }
